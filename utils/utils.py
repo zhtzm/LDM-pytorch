029884d3e65164ca127a6a2ea09c803fbf7c029a
zhtzm <htzhou04@gmail.com>
2024-08-01 03:28:09 +0800

    modify

--- utils/utils.py
+++ utils/utils.py
@@ -7,7 +7,7 @@ from torch import nn
 from torch.utils.data import random_split, DataLoader
 from torchvision import transforms
 
-from models.modules.ema import EMA
+from models.ema import EMA
 
 
 def parse_yaml_config(config_path: str):
